94b695b81d57ad4cb86eaf50b5e7317f70353808
Baranidharan <135943230+baranishot@users.noreply.github.com>
2025-03-06 00:05:45 +0530

    Update DatabaseServiceProjectApplicationTests.java

--- src/test/java/com/javaproject/DatabaseServiceProjectApplicationTests.java
+++ src/test/java/com/javaproject/DatabaseServiceProjectApplicationTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 @AutoConfigureMockMvc
